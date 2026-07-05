f0fe067c64621257f5f6b0d18ce558e974832850
StardustEnigma <atharvamandle19@gmail.com>
2026-07-05 16:48:25 +0000

    login test done for handler

--- handler/auth_handler_test.go
+++ handler/auth_handler_test.go
@@ -33,7 +33,10 @@ func (m *MockAuthService) CreateUser(ctx context.Context,request dto.Register)(m
 
 
 func (m *MockAuthService) LoginUser(ctx context.Context,request dto.LoginRequest)(string,error){
-
+	if m.shouldErr{
+		return " ",errors.New("Service error")
+	}
+	return "mock-jwt-token",nil
 }
 
 func TestRegisterUser(t *testing.T){
@@ -79,6 +82,51 @@ func TestRegisterUser(t *testing.T){
 			handler.RegisterUser(w,req)
 
 			if w.Code!= tt.expectedStatus {
+				t.Errorf("Expected status %d got %d",tt.expectedStatus,w.Code)
+			}
+		})
+	}
+}
+
+func TestLoginUser(t *testing.T){
+	tests := []struct{
+		name string
+		request dto.LoginRequest
+		mockError bool
+		expectedStatus int
+
+	}{
+		{
+			name:"Valid login",
+			request :dto.LoginRequest{
+				Username: "john123",
+				Password: "password123",
+			},
+			mockError : false,
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name: "invalid credentials",
+			request: dto.LoginRequest{
+				Username: "john123",
+				Password: "incorrect-password",
+			},
+			mockError: true,
+			expectedStatus: http.StatusBadRequest,
+		},
+	}
+	for _,tt := range tests{
+		t.Run(tt.name,func(t *testing.T) {
+			body,_:= json.Marshal(tt.request)
+			req := httptest.NewRequest("POST","/login",bytes.NewReader(body))
+			w := httptest.NewRecorder()
+			mockService := &MockAuthService{
+				shouldErr: tt.mockError,
+			}
+			handler := &Handler{AuthService: mockService}
+			handler.LoginUser(w,req)
+
+			if w.Code != tt.expectedStatus {
 				t.Errorf("Expected status %d got %d",tt.expectedStatus,w.Code)
 			}
 		})
