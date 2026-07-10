6c05bf0056c0b026d2bddfa6f5746de0e8ffaffc
StardustEnigma <atharvamandle19@gmail.com>
2026-07-10 03:54:32 +0000

    update Password done

---
+++ dto/ChangePassword.go
@@ -0,0 +1,7 @@
+package dto
+
+type ChangePasswordRequest struct{
+	Username string `json:"username"`
+	OldPassword string `json:"old_password"`
+	NewPassword string `json:"new_password"`
+}
--- services/user_service.go
+++ services/user_service.go
@@ -8,11 +8,13 @@ import (
 	"github.com/StardustEnigma/AuthGo/models"
 	"github.com/StardustEnigma/AuthGo/repository"
 	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
 )
 
 type UserService interface{
 	GetUserProfile(ctx context.Context,userId int)(models.User,error)
 	UpdateUserProfile(ctx context.Context,userId int,request dto.UpdateRequest)(string,error)
+	ChangeUserPassword(ctx context.Context,userId int,request dto.ChangePasswordRequest)(string,error)
 }
 
 type userService struct{
@@ -54,4 +56,20 @@ func (r *userService) UpdateUserProfile(ctx context.Context,userId int,request d
 		return "",update
 	}
 	return "Updated Username Successfully",nil
+}
+
+func(r *userService) ChangeUserPassword(ctx context.Context,userId int,request dto.ChangePasswordRequest)(string,error){
+ user,err := r.Repo.GetUserById(ctx,userId)
+ if err != nil {
+	return "",errors.New("User does not exists")
+ }
+ if user.IsSuspended || !user.IsActive {
+		return "",errors.New("User is either suspended or not verified")
+	}
+
+ ok := bcrypt.CompareHashAndPassword([]byte(user.Password),[]byte(request.OldPassword))
+ if ok != nil {
+	return "",errors.New("Invalid Old password")
+
+}
 }
