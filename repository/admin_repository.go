a1dd8277d67012d6c8bf3fca418d2dd945c5f0b0
StardustEnigma <atharvamandle19@gmail.com>
2026-07-14 17:01:33 +0000

    admin service create admin done

--- repository/admin_repository.go
+++ repository/admin_repository.go
@@ -6,16 +6,32 @@ import (
 	"github.com/StardustEnigma/AuthGo/models"
 )
 
-
-type AdminRepository interface{
-	CreateAdmin(ctx context.Context,user models.User)(models.User,error)
+type AdminRepository interface {
+	CreateAdmin(ctx context.Context, user models.User) (models.User, error)
 }
 
-func (r *Repository)CreateAdmin(ctx context.Context,user models.User)(models.User,error){
+func (r *Repository) CreateAdmin(ctx context.Context, user models.User) (models.User, error) {
 	query := `INSERT INTO users
 				(username,password,created_at,role)
 				VALUES ($1,$2,$3,$4)
 				RETURNING user_id,username,password,created_at,role`
 	var savedAdmin models.User
-	
+	err := r.DB.QueryRowContext(
+		ctx,
+		query,
+		user.UserName,
+		user.Password,
+		user.CreatedAt,
+		user.Role,
+	).Scan(
+		&savedAdmin.UserId,
+		&savedAdmin.UserName,
+		&savedAdmin.Password,
+		&savedAdmin.CreatedAt,
+		&savedAdmin.Role,
+	)
+	if err != nil {
+		return models.User{}, err
+	}
+	return savedAdmin, nil
 }
